f93e698838eb3f33f38e5cdcdd58e624b1c03e9a
sentictyn <103776389+sentictyn@users.noreply.github.com>
2023-02-14 17:01:10 +0100

    Update srogzy.js

--- srogzy.js
+++ srogzy.js
@@ -1,9 +1,9 @@
 // this is my firs console log
 
-console.log(""Hello Word")
+console.log(""Hello Word"),
 
 // this is my 2nd console log
 
-console.log("Hello Word")
+console.log("Hello Word"),
             
-message.channel.send("Hello Word")
+message.channel.send("Hello Word"),
